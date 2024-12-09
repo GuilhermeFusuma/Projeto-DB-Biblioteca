7f1413ece792372112374d0344e85ca19b298ef0
guilherme kenji fusuma <fuskenji@gmail.com>
2024-12-09 08:47:20 -0300

    começado popup do verLivros

--- insert into biblioteca.sql
+++ insert into biblioteca.sql
@@ -94,6 +94,43 @@ VALUES
 (16, 'Futebol'),             -- Categoria 'Esportes'
 (16, 'Atletismo');    
 
+INSERT INTO Assuntos (ID_GeneroCategoria, Nome)
+VALUES
+(1, 'Viagens Interplanetárias'),   -- Gênero 'Ficção Científica'
+(1, 'Robôs e Autômatos'),          -- Gênero 'Ficção Científica'
+(2, 'Mitologia e Lendas'),         -- Gênero 'Fantasia'
+(2, 'Dragões e Magia'),            -- Gênero 'Fantasia'
+(3, 'Romances Policiais'),         -- Gênero 'Mistério'
+(3, 'Casos de Assassinato'),       -- Gênero 'Mistério'
+(4, 'Comédias Românticas'),        -- Gênero 'Romance'
+(4, 'Triângulos Amorosos'),        -- Gênero 'Romance'
+(5, 'Terror Gótico'),              -- Gênero 'Terror'
+(5, 'Criaturas Sobrenaturais'),    -- Gênero 'Terror'
+(6, 'Dramas de Família'),          -- Gênero 'Drama'
+(6, 'Narrativas Emocionais'),      -- Gênero 'Drama'
+(7, 'Exploração de Continentes'),  -- Gênero 'Aventura'
+(7, 'Jornadas de Descoberta'),     -- Gênero 'Aventura'
+(8, 'Grandes Guerras Mundiais'),   -- Gênero 'Histórico'
+(8, 'Biografias de Reis e Rainhas'); -- Gênero 'Histórico'
+
+INSERT INTO Assuntos (ID_GeneroCategoria, Nome)
+VALUES
+(9, 'Clássicos da Literatura'),   -- Categoria 'Ficção'
+(9, 'Romances Distópicos'),       -- Categoria 'Ficção'
+(10, 'Memórias e Biografias'),    -- Categoria 'Não Ficção'
+(10, 'Relatos de Viagens'),       -- Categoria 'Não Ficção'
+(11, 'Ciência da Computação'),    -- Categoria 'Acadêmico'
+(11, 'Engenharia Elétrica'),      -- Categoria 'Acadêmico'
+(12, 'Medicina Geral'),           -- Categoria 'Saúde e Bem-Estar'
+(12, 'Psicologia e Autoajuda'),   -- Categoria 'Saúde e Bem-Estar'
+(13, 'Gestão de Projetos'),       -- Categoria 'Negócios e Economia'
+(13, 'Planejamento Estratégico'), -- Categoria 'Negócios e Economia'
+(14, 'História da Pintura'),      -- Categoria 'Arte e Design'
+(14, 'Arquitetura Clássica'),     -- Categoria 'Arte e Design'
+(15, 'Estudos sobre Religiões'),  -- Categoria 'Religião'
+(15, 'Espiritualidade e Fé'),     -- Categoria 'Religião'
+(16, 'História do Futebol'),      -- Categoria 'Esportes'
+(16, 'Guias de Treinamento');     -- Categoria 'Esportes'
 
 SELECT *
 FROM GeneroCategoria
